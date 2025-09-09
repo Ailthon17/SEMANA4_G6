55f13e8d6833453f4ee74bdb52e948592ca0bd9e
Ailthon17 <ailthonalejandrooconleyva@gmail.com>
2025-09-09 08:41:35 -0500

    lo mio de mi

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("INGRESA EL VALOR DE A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("INGRESA EL VALOR DE B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            if (A > B)
+            {
+                Console.WriteLine("EXECLENYTE ");
+
+            }
+            else if (A < B)
+            {
+                Console.WriteLine(" BRUTO ");
+
+
+
+
+
+
+            }
+        }
+    }
+}
